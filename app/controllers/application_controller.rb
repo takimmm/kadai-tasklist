b502714e084a55a520a8a3d2c5e5e8ba7677c2fd
taki <taishusen12@gmail.com>
2022-04-02 06:56:32 +0000

    CRUD tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,53 @@
 class ApplicationController < ActionController::Base
+  def index
+      @tasks = Task.all
+  end
+
+  def show
+      @task = Task.find(params[:id])
+  end
+
+  def new
+      @atsk = Task.new
+  end
+
+  def create
+      @task = Task.new(task_params)
+      if @task.save
+          flash[:success] = 'タスクが正常に作成されました。'
+          redirect_to @task
+      else 
+          flash.now[:danger] = 'タスクが作成されませんでした。'
+          render :new
+      end
+          
+  end
+
+  def edit
+      @task = Task.find(params[:id])
+  end
+
+  def update
+      @task = Task.update(task_params)
+      if @task.update(message_params)
+          flash[:success] = 'タスクが正常に更新されました。'
+          redirect_to @task
+      else 
+          flash.now[:danger] = 'タスクが更新されませんでした。'
+          render :edit
+      end
+  end
+
+  def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      
+      flash[:success] = 'タスクは正常に削除されました。'
+      redirect_to tasks_url
+  end
+  
+  private
+  def message_params
+      params.require(:message).permit(:content)
+  end
 end
