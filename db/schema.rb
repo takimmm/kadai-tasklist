17f39f2cde8d52be8d0b68b4fdca0ae287ff4121
taki <taishusen12@gmail.com>
2022-04-03 02:06:10 +0000

    improved

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_111005) do
+ActiveRecord::Schema.define(version: 2022_04_03_013837) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
