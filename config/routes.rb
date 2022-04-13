b7c6a771ba66221bad421d4256dd351f6689b19a
taki <taishusen12@gmail.com>
2022-04-13 06:25:47 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
+  resources :users, only: [:create]
 end
